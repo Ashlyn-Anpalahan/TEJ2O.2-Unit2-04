ba7222d68c390f5a396f3ad8586ca769d9cd6225
Ashlyn-Anpalahan <ashlyn.anpalahan@stu.ocsb.ca>
2026-02-20 18:45:36 -0500

    Changed to say temperature

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ashlyn
+ * Created on: Feb 2026
+ * This program will show the current temperature
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    let temp = input.temperature()
+    basic.showString("The temperature is:" + (temp) + "C")
+})
