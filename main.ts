cd68c7d94a7a09c60de232fc91ce9ba478b90dc6
Ashlyn-Anpalahan <ashlyn.anpalahan@stu.ocsb.ca>
2026-02-24 15:20:47 -0500

    Changed code

--- main.ts
+++ main.ts
@@ -4,3 +4,13 @@
 * Created on: Feb 2026
 * This program will show the current temperature
 */
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    const temperature = input.temperature()
+    basic.clearScreen()
+    basic.showString("The temperature is:" + (temperature) + "C.")
+})
+
